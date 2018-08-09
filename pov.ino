38540f89a5edeca6cc1760a87c49209a335596fc
siweiyu <sevenysw@126.com>
2018-08-09 22:02:59 +0800

    Display the word after detect the stick
    Following work: place a stick before every mirror, such that the beginning point for each mirror is accuarate

--- pov.ino
+++ pov.ino
@@ -1,7 +1,7 @@
 
 // an project done by Glen p yeldho 
 // for more info and updates  https://www.facebook.com/glenp.yeldho
-
+#include <math.h>
 int _[] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0};
 int A[] = {0,1,1,1,1, 1,0,1,0,0, 0,1,1,1,1};
 int A_[] = {1,0,0,0,0, 1,0,0,0,0, 1,0,0,0,0};
@@ -31,11 +31,12 @@ int X[] = {1,1,0,1,1, 0,0,1,0,0, 1,1,0,1,1};
 int Y[] = {1,1,0,0,0, 0,0,1,0,0, 1,1,1,1,1};
 int Z[] = {1,0,0,1,1, 1,0,1,0,1, 1,1,0,0,1};
 
-float v = 50.8;  //round per minute
+float v = 1/0;  //round per minute
 const int echo = 9;        // 反馈信号
 int letterSpace;
 int dotTime;
-float k = 0.5*1.061;
+float k = 0.5*1.02;
+bool flag = 1;
 void setup()
 {
   // setting the ports of the leds to OUTPUT
@@ -57,7 +58,7 @@ void checkv(void){
     IntervalTime=pulseIn(echo, HIGH);//用自带的函数采样反馈的高电平的宽度，单位ms，电机模块有个圆盘，盘上有4个条形孔
     t2 = pulseIn(echo,HIGH);
     IntervalTime = IntervalTime + t2;
-    v=60000000.0/IntervalTime; //使用浮点计算出r/min
+    v=30000000.0/IntervalTime; //使用浮点计算出r/min
 }
 
 void printLetter(int letter[])
@@ -69,7 +70,7 @@ void printLetter(int letter[])
   {
     digitalWrite(8-(y+2), 1);
   }
-  delay(letterSpace);
+  delay(1*letterSpace);
   
   // printing the first y row of the letter
   for (y=0; y<5; y++)
@@ -97,21 +98,36 @@ void printLetter(int letter[])
   {
     digitalWrite(8-(y+2), 1);
   }
-  delay(letterSpace);
+  delay(1*letterSpace);
 }
 void loop()
 {
   delay(2000);
-  checkv();
+  while (isinf(v) || v==-1)
+    {
+      delay(1000);
+      checkv();
+    }
+  int i;
   
-  letterSpace = 50000.0/19.0/v*k;
-  dotTime = 30000.0/19.0/v*k;
+  letterSpace = 7*10000.0/19.0/v*k; //2 letterSpace
+  dotTime = (19-7*2.0)/3*10000.0/19.0/v*k;     //3 dotTime
   Serial.println(v);
   Serial.println(letterSpace);
   Serial.println(dotTime);
   
   while(true){
-     printLetter(C);
+    //input is HIGH when across nothing. LOW when across the stick.
+    //Serial.println(digitalRead(echo));
+    flag = digitalRead(echo);
+    if (flag == 0)
+    {
+      for (i=0;i<6;i++)
+        {
+          
+          printLetter(C);
+        }
+    }
   }
   //printLetter(N);
   //printLetter(_);
