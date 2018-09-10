8cfd4bf306adfa69c0bd1eb09c03679d8da4616c
siweiyu <sevenysw@126.com>
2018-09-10 22:24:35 +0800

    I can show one letter changing with time
    Next step: show multi letter per row

--- pov.ino
+++ pov.ino
@@ -30,13 +30,26 @@ int W[] = {1,1,1,1,0, 0,0,1,1,0, 1,1,1,1,0};
 int X[] = {1,1,0,1,1, 0,0,1,0,0, 1,1,0,1,1};
 int Y[] = {1,1,0,0,0, 0,0,1,0,0, 1,1,1,1,1};
 int Z[] = {1,0,0,1,1, 1,0,1,0,1, 1,1,0,0,1};
+int TT[] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0};
+int AS[][15] = {{0,1,1,1,1, 1,0,1,0,0, 0,1,1,1,1},{1,1,1,1,1, 1,0,1,0,1, 0,1,0,1,0},{0,1,1,1,0, 1,0,0,0,1, 1,0,0,0,1},
+                  {1,1,1,1,1, 1,0,0,0,1, 0,1,1,1,0},{1,1,1,1,1, 1,0,1,0,1, 1,0,1,0,1},{1,1,1,1,1, 1,0,1,0,0, 1,0,1,0,0},
+                  {0,1,1,1,0, 1,0,1,0,1, 0,0,1,1,0},{1,1,1,1,1, 0,0,1,0,0, 1,1,1,1,1},{0,0,0,0,1, 1,0,1,1,1, 0,0,0,0,1},
+                  {1,0,0,0,0, 1,0,0,0,1, 1,1,1,1,1},{1,1,1,1,1, 0,0,1,0,0, 0,1,0,1,1},{1,1,1,1,1, 0,0,0,0,1, 0,0,0,0,1},
+                  {1,1,1,1,1, 0,1,1,0,0, 0,1,1,1,1},{1,1,1,1,1, 1,0,0,0,0, 0,1,1,1,1},{0,1,1,1,0, 1,0,0,0,1, 0,1,1,1,0},
+                  {1,1,1,1,1, 1,0,1,0,0, 0,1,0,0,0},{0,1,1,1,1, 1,0,0,1,1, 0,1,1,1,1},{1,1,1,1,1, 1,0,1,0,0, 0,1,0,1,1},
+                  {0,1,0,0,1, 1,0,1,0,1, 1,0,0,1,0},{1,0,0,0,0, 1,1,1,1,1, 1,0,0,0,0},{1,1,1,1,1, 0,0,0,0,1, 1,1,1,1,1},
+                  {1,1,1,1,0, 0,0,0,0,1, 1,1,1,1,0},{1,1,1,1,0, 0,0,1,1,0, 1,1,1,1,0},{1,1,0,1,1, 0,0,1,0,0, 1,1,0,1,1},
+                  {1,1,0,0,0, 0,0,1,0,0, 1,1,1,1,1},{1,0,0,1,1, 1,0,1,0,1, 1,1,0,0,1}};
 
 float v = 1/0;  //round per minute
 const int echo = 9;        // 反馈信号
 int letterSpace;
 int dotTime;
-float k = 0.5*1.02;
+float k = 0.62; // right -> increase
+float period = 0;
 bool flag = 1;
+int n_lett = 2; // how many letters
+int n_mirr = 6;
 void setup()
 {
   // setting the ports of the leds to OUTPUT
@@ -56,6 +69,7 @@ void checkv(void){
     long IntervalTime=0; //定义一个时间变量
     long t2 = 0;
     IntervalTime=pulseIn(echo, HIGH);//用自带的函数采样反馈的高电平的宽度，单位ms，电机模块有个圆盘，盘上有4个条形孔
+    
     t2 = pulseIn(echo,HIGH);
     IntervalTime = IntervalTime + t2;
     v=30000000.0/IntervalTime; //使用浮点计算出r/min
@@ -64,13 +78,6 @@ void checkv(void){
 void printLetter(int letter[])
 {
   int y;
-
- // printing the sspace between the letters
-  for (y=0; y<5; y++)
-  {
-    digitalWrite(8-(y+2), 1);
-  }
-  delay(1*letterSpace);
   
   // printing the first y row of the letter
   for (y=0; y<5; y++)
@@ -98,7 +105,7 @@ void printLetter(int letter[])
   {
     digitalWrite(8-(y+2), 1);
   }
-  delay(1*letterSpace);
+  delay(0.3*letterSpace);
 }
 void loop()
 {
@@ -109,28 +116,40 @@ void loop()
       checkv();
     }
   int i;
-  
-  letterSpace = 7*10000.0/19.0/v*k; //2 letterSpace
-  dotTime = (19-7*2.0)/3*10000.0/19.0/v*k;     //3 dotTime
+  //v : round / min
+  // time: ms
+  // time per round: 
+  period = 60000/v*k/n_mirr/n_lett;
+  letterSpace = 1.0/4.0*period; //1 letterSpace
+  dotTime = (period-letterSpace)/3;     //3 dotTime
   Serial.println(v);
   Serial.println(letterSpace);
   Serial.println(dotTime);
-  
+  int k = 0;
+  int ind = 0;
   while(true){
     //input is HIGH when across nothing. LOW when across the stick.
-    //Serial.println(digitalRead(echo));
+    
     flag = digitalRead(echo);
     if (flag == 0)
     {
-      for (i=0;i<6;i++)
-        {
-          
-          printLetter(C);
-        }
+      k = k + 1;
+      if (k==10)
+      {
+        k=0;
+        
+        ind = ind + 1;
+        if (ind==26)
+          ind = 0;
+      }
+
+      printLetter(_);
+      printLetter(AS[ind]);
+
+      
+
     }
   }
-  //printLetter(N);
-  //printLetter(_);
   
 }
  
